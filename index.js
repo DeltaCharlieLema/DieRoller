100713365348a3faa4412e4ec10cb963757ece26
Delta Lema <180664295+DeltaCharlieLema@users.noreply.github.com>
2025-08-03 18:49:32 -0500

    added the option to select different dies

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Happy developing ✨')
+const result = document.querySelector('#result');
+const rollButton = document.querySelector('#roll-button');
+const dieSelector = document.querySelector("#die-selection")
+const d4 = document.querySelector("#d4")
+const dieImage = document.querySelector("#die-image")
+
+
+function roll() {
+  result.textContent = Math.floor(Math.random() * dieSelector.value) + 1;
+}
+rollButton.addEventListener('click', () => {
+  roll();
+});
+
+dieSelector.addEventListener("change", () => {
+  dieImage.src = `img/d${dieSelector.value}.png`
+  roll();
+  console.log(dieSelector.value);
+})
